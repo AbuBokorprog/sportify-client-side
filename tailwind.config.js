f51f502be1a371d517ad2cdacc676159c3991a90
AbuBokorprog <abubokor1066@gmail.com>
2023-10-17 21:49:34 +0600

    Main branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#000000", //Black
+
+          secondary: "#06b6d4", //Cyan
+
+          info: "#67e8f9", //white-Cyan
+
+          neutral: "#21323b",
+
+          "base-100": "#FFFFFF",
+
+          accent: "#b3bde6",
+
+          success: "#25d497",
+
+          warning: "#f9aa48",
+
+          error: "#ea4354",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 };
