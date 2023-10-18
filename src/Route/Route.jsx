c9ce1bdc721a4e81be12b7c84dc409ffe2680050
unknown <eshansaif1234@gmail.com>
2023-10-18 16:39:31 +0600

    Login and registration routes and components created

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../Compound/Home/Home";
 import Main from "../Layout/Main/Main";
+import Login from "../Compound/Login/Login";
+import Register from "../Compound/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      }
     ],
   },
 ]);
