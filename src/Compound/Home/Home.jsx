8dd2bd5ef72fe7a59d5b214388320a0427b81828
AbuBokorprog <abubokor1066@gmail.com>
2023-10-18 16:39:02 +0600

    marque section added

--- src/Compound/Home/Home.jsx
+++ src/Compound/Home/Home.jsx
@@ -3,11 +3,13 @@ import Banner from "./Banner";
 import TopCourses from "./TopCourses/TopCourses";
 import TopInstructor from "./TopInstructor/TopInstructor";
 import Gallery from "./Gallery";
+import Partner from "./Partner";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Partner></Partner>
       <TopCourses></TopCourses>
       <TopInstructor></TopInstructor>
       <Gallery></Gallery>
---
+++ src/Compound/Home/Partner.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const Partner = () => {
+  return (
+    <div className="my-20">
+      <Marquee>
+        <img className="w-40 px-4" src="/apple.png" alt="" />
+        <img className="w-40 px-4" src="/hp.png" alt="" />
+        <img className="w-40 px-4" src="/social.png" alt="" />
+        <img className="w-40 px-4" src="/search.png" alt="" />
+        <img className="w-40 px-4" src="/walmart.png" alt="" />
+        <img className="w-40 px-4" src="/samsung.png" alt="" />
+        <img className="w-40 px-4" src="/ibm.png" alt="" />
+        <img className="w-40 px-4" src="/microsoft.png" alt="" />
+        <img className="w-40 px-4" src="/brand.png" alt="" />
+      </Marquee>
+    </div>
+  );
+};
+
+export default Partner;
