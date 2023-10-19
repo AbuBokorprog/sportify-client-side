b2d013482d4b7eaf79a7be7b41f2d7ba81915920
AbuBokorprog <abubokor1066@gmail.com>
2023-10-19 07:32:48 +0600

    All push in main branch

--- src/Compound/Courses/Coursespage.jsx
+++ src/Compound/Courses/Coursespage.jsx
@@ -1,29 +1,28 @@
 import React, { useEffect, useState } from "react";
 import CourseCard from "../Home/TopCourses/Coursecard";
 
-
 const Coursespages = () => {
-    const [topCourses, setTopCourses] = useState([]);
-    useEffect(() => {
-        fetch("/TopCourses.json")
-            .then((res) => res.json())
-            .then((data) => {
-                setTopCourses(data);
-            });
-    }, []);
+  const [topCourses, setTopCourses] = useState([]);
+  useEffect(() => {
+    fetch("/TopCourses.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setTopCourses(data);
+      });
+  }, []);
 
-    return (
-        <div className="my-20  ">
-            <p className="text-3xl font-bold ms-72 mb-20 ">All Course :</p>
-            <div className="flex flex-col justify-center items-center">
-                <div className="grid lg:grid-cols-3 gap-5 mx-auto">
-                    {topCourses.map((C) => (
-                        <CourseCard key={C.id} course={C}></CourseCard>
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className="my-20  ">
+      <p className="text-3xl text-left ms-10 mb-2 font-bold ">All Course :</p>
+      <div className="flex flex-col justify-center items-center">
+        <div className="grid lg:grid-cols-3 gap-5 mx-auto">
+          {topCourses.map((C) => (
+            <CourseCard key={C.id} course={C}></CourseCard>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Coursespages;
