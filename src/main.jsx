4bc7896cf10267b6b2e10bb7d52bf9232b79a2b7
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-10-19 10:23:33 +0600

    will add axios

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import router from "./Route/Route.jsx";
-import { RouterProvider } from "react-router-dom";
+import {RouterProvider} from "react-router-dom";
 import AuthProvider from "./providers/AuthProvider";
+
+// Globally used baseURL with JWT
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider><RouterProvider router={router} /></AuthProvider>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
