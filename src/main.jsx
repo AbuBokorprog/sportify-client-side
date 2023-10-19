77fb471486e6cd564252abc5e80b9dcc79d92fc7
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-10-19 10:10:25 +0600

    auth integrated between backend

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import router from "./Route/Route.jsx";
-import { RouterProvider } from "react-router-dom";
+import {RouterProvider} from "react-router-dom";
 import AuthProvider from "./providers/AuthProvider";
+import axios from "axios";
+
+// Globally used baseURL with JWT
+axios.defaults.baseURL = "http://localhost:3000";
+
+let token = localStorage.getItem("token") || "";
+token = token && `Bearer ${token}`;
+
+axios.defaults.headers.common["Authorization"] = token;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider><RouterProvider router={router} /></AuthProvider>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
