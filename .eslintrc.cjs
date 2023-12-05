b57fb9893ac848e131b162de4c1865770fdb532e
AbuBokorprog <abubokor1066@gmail.com>
2023-12-05 17:00:22 +0600

    eslint error solved

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,12 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "extends:eslint:recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
